d5cf4b3ca89fc531b8149c710fdbdb7764253067
vineetachauhan <contact.vineetarajput@gmail.com>
2021-12-10 22:51:22 +0530

    added password properties in test file to secure the web app

--- sampleProj/src/sampleProj/test.java
+++ sampleProj/src/sampleProj/test.java
@@ -2,6 +2,15 @@ package sampleProj;
 
 public class test {
 	private String userName;
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getUserName() {
 		return userName;
